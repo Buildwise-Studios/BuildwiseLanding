272e34d9a2c50d40c18073099e7b86b3f33f02ec
Charles <charles.fauchet@gmail.com>
2025-07-02 13:02:04 +0800

    update index file for production

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,22 @@
 import { createServer } from 'vite'
 
-const server = await createServer({
-  configFile: './vite.config.ts',
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: true
-  }
-})
+async function startServer() {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('You are in production mode. Static assets will be served by Vercel.');
+  } else {
+    const server = await createServer({
+      configFile: './vite.config.ts',
+      server: {
+        host: '0.0.0.0',
+        port: 5000,
+        allowedHosts: true
+      }
+    });
 
-await server.listen()
-console.log('Frontend server running on port 5000')
-server.printUrls()
+    await server.listen();
+    console.log('Frontend server running on port 5000');
+    server.printUrls();
+  }
+}
+
+startServer();
